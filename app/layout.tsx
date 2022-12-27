89b10acf9ff5903f208bca579cb2c9b445fc19c0
vugomars <vugomars@gmail.com>
2022-12-27 13:54:50 +0700

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
-import './globals.css'
+import { Footer, Header, Warning } from "@components/common";
+import Providers from "@context/Providers";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +14,14 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Providers>
+          <Warning />
+          <Header />
+          {children}
+          <Footer />
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
